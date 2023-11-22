f9816a51d81b17eb549bfcd49e6d89e75a5d18c9
Marcin Chrost <mchrost82@gmail.com>
2023-11-22 20:23:33 +0100

    AOP @Retry annotation

--- src/main/java/eu/chrost/shop/payments/FakePaymentService.java
+++ src/main/java/eu/chrost/shop/payments/FakePaymentService.java
@@ -1,5 +1,6 @@
 package eu.chrost.shop.payments;
 
+import eu.chrost.shop.common.Retry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,8 @@ public class FakePaymentService implements PaymentService {
     private final PaymentRepository paymentRepository;
 
     @LogPayments
+    @Retry(attempts = 2)
+    @Override
     public Payment process(PaymentRequest paymentRequest) {
         var payment = Payment.builder()
                 .id(paymentIdGenerator.getNext())
