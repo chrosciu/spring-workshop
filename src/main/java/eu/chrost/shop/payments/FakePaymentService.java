8506c3e9186da14a1f37db6beabb3639f6cd0be4
Marcin Chrost <mchrost82@gmail.com>
2023-11-22 14:46:53 +0100

    PaymentRepository

--- src/main/java/eu/chrost/shop/payments/FakePaymentService.java
+++ src/main/java/eu/chrost/shop/payments/FakePaymentService.java
@@ -9,14 +9,16 @@ import java.time.Instant;
 @Component
 public class FakePaymentService implements PaymentService {
     private final PaymentIdGenerator paymentIdGenerator;
+    private final PaymentRepository paymentRepository;
 
     @LogPayments
     public Payment process(PaymentRequest paymentRequest) {
-        return Payment.builder()
+        var payment = Payment.builder()
                 .id(paymentIdGenerator.getNext())
                 .money(paymentRequest.getMoney())
                 .timestamp(Instant.now())
                 .status(PaymentStatus.STARTED)
                 .build();
+        return paymentRepository.save(payment);
     }
 }
