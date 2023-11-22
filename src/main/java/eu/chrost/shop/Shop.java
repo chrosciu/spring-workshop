8e82fe740df6c8003bcf89f7ccf2c293e009815d
Marcin Chrost <mchrost82@gmail.com>
2023-11-22 14:14:09 +0100

    Spring AOP

--- src/main/java/eu/chrost/shop/Shop.java
+++ src/main/java/eu/chrost/shop/Shop.java
@@ -1,7 +1,7 @@
 package eu.chrost.shop;
 
-import eu.chrost.shop.payments.LoggingPaymentService;
 import eu.chrost.shop.payments.PaymentRequest;
+import eu.chrost.shop.payments.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 public class Shop {
     public static void main(String[] args) {
         try (var applicationContext = new AnnotationConfigApplicationContext("eu.chrost.shop")) {
-            var paymentService = applicationContext.getBean(LoggingPaymentService.class);
+            var paymentService = applicationContext.getBean(PaymentService.class);
             var paymentRequest = PaymentRequest.builder()
                     .money(BigDecimal.valueOf(100))
                     .build();
