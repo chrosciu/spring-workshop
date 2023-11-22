5aa4fa822265aa6039a26aa838d584c57339b7d8
Marcin Chrost <mchrost82@gmail.com>
2023-11-22 11:50:59 +0100

    Spring container

--- src/main/java/eu/chrost/shop/Shop.java
+++ src/main/java/eu/chrost/shop/Shop.java
@@ -1,23 +1,22 @@
 package eu.chrost.shop;
 
-import eu.chrost.shop.payments.FakePaymentService;
-import eu.chrost.shop.payments.IncrementalPaymentIdGenerator;
 import eu.chrost.shop.payments.LoggingPaymentService;
 import eu.chrost.shop.payments.PaymentRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.math.BigDecimal;
 
 @Slf4j
 public class Shop {
     public static void main(String[] args) {
-        var paymentIdGenerator = new IncrementalPaymentIdGenerator();
-        var fakePaymentService = new FakePaymentService(paymentIdGenerator);
-        var paymentService = new LoggingPaymentService(fakePaymentService);
-        var paymentRequest = PaymentRequest.builder()
-                .money(BigDecimal.valueOf(100))
-                .build();
-        var payment = paymentService.process(paymentRequest);
-        log.info("{}", payment);
+        try (var applicationContext = new AnnotationConfigApplicationContext("eu.chrost.shop")) {
+            var paymentService = applicationContext.getBean(LoggingPaymentService.class);
+            var paymentRequest = PaymentRequest.builder()
+                    .money(BigDecimal.valueOf(100))
+                    .build();
+            var payment = paymentService.process(paymentRequest);
+            log.info("{}", payment);
+        }
     }
 }
