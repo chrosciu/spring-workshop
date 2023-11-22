8faf771fdaa6db55ef032ff52f777552d2532950
Marcin Chrost <mchrost82@gmail.com>
2023-11-22 14:14:09 +0100

    Spring AOP

---
+++ src/main/java/eu/chrost/shop/payments/PaymentsConfiguration.java
@@ -0,0 +1,9 @@
+package eu.chrost.shop.payments;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class PaymentsConfiguration {
+}
