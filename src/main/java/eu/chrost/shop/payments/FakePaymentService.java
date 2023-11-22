bcbf685d589b33a70e7aada072dae5326971939b
Marcin Chrost <mchrost82@gmail.com>
2023-11-22 10:52:03 +0100

    Separation with proxy

--- src/main/java/eu/chrost/shop/payments/FakePaymentService.java
+++ src/main/java/eu/chrost/shop/payments/FakePaymentService.java
@@ -1,23 +1,19 @@
 package eu.chrost.shop.payments;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 import java.time.Instant;
 
-@Slf4j
 @RequiredArgsConstructor
-public class FakePaymentService {
+public class FakePaymentService implements PaymentService {
     private final PaymentIdGenerator paymentIdGenerator;
 
     public Payment process(PaymentRequest paymentRequest) {
-        var payment = Payment.builder()
+        return Payment.builder()
                 .id(paymentIdGenerator.getNext())
                 .money(paymentRequest.getMoney())
                 .timestamp(Instant.now())
                 .status(PaymentStatus.STARTED)
                 .build();
-        log.info("A new payment of {} has been initiated", payment.getMoney());
-        return payment;
     }
 }
