6ba0e249b379a7429f38eca69077adb3149f751b
Marcin Chrost <mchrost82@gmail.com>
2023-11-22 17:34:48 +0100

    @Qualifier

--- src/main/java/eu/chrost/shop/payments/IncrementalPaymentIdGenerator.java
+++ src/main/java/eu/chrost/shop/payments/IncrementalPaymentIdGenerator.java
@@ -1,9 +1,11 @@
 package eu.chrost.shop.payments;
 
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("incremental")
 public class IncrementalPaymentIdGenerator implements PaymentIdGenerator {
 
     private static final String ID_FORMAT = "%010d";
