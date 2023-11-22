702705fc8ee9bb0d87d13613b1daf1b1ed49c9f5
Marcin Chrost <mchrost82@gmail.com>
2023-11-22 15:05:42 +0100

    Around aspect example

--- src/main/java/eu/chrost/shop/payments/PaymentConsoleLogger.java
+++ src/main/java/eu/chrost/shop/payments/PaymentConsoleLogger.java
@@ -1,9 +1,13 @@
 package eu.chrost.shop.payments;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
 
 @Component
 @Aspect
@@ -14,5 +18,15 @@ public class PaymentConsoleLogger {
     @AfterReturning(value = "@annotation(LogPayments)", returning = "payment")
     public void log(Payment payment) {
         log.info(LOG_FORMAT, payment);
+    }
+
+    @Around(value = "@annotation(LogPayments) && args(paymentRequest)")
+    public Object aroundPayment(ProceedingJoinPoint proceedingJoinPoint, PaymentRequest paymentRequest) throws Throwable {
+        log.info("Around payment: {}", paymentRequest);
+        if (paymentRequest.getMoney().compareTo(BigDecimal.valueOf(10)) < 0) {
+            //throw new IllegalStateException("Biednych nie obslugujemy");
+            return Payment.builder().status(PaymentStatus.FAILED).build();
+        }
+        return proceedingJoinPoint.proceed();
     }
 }
