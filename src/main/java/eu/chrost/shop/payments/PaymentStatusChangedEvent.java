36093c84845dd073bb35d56fcce345cd1e088ebc
Marcin Chrost <mchrost82@gmail.com>
2023-11-23 11:20:09 +0100

    Event publisher & listener

---
+++ src/main/java/eu/chrost/shop/payments/PaymentStatusChangedEvent.java
@@ -0,0 +1,14 @@
+package eu.chrost.shop.payments;
+
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+public class PaymentStatusChangedEvent extends ApplicationEvent {
+    @Getter
+    private final Payment payment;
+
+    public PaymentStatusChangedEvent(Object source, Payment payment) {
+        super(source);
+        this.payment = payment;
+    }
+}
