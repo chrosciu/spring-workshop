d6d59b86cbe99cd5c79f61d9bfb5ca3f3abc5c5e
Marcin Chrost <mchrost82@gmail.com>
2023-11-23 17:43:00 +0100

    Reading messages from properties

--- src/main/java/eu/chrost/shop/products/ProductNotFoundException.java
+++ src/main/java/eu/chrost/shop/products/ProductNotFoundException.java
@@ -1,4 +1,10 @@
 package eu.chrost.shop.products;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class ProductNotFoundException extends RuntimeException {
+    @Getter
+    private final long id;
 }
--- src/main/java/eu/chrost/shop/products/ProductsControllerAdvice.java
+++ src/main/java/eu/chrost/shop/products/ProductsControllerAdvice.java
@@ -1,16 +1,24 @@
 package eu.chrost.shop.products;
 
 import eu.chrost.shop.common.ExceptionDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Locale;
+
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
+@RequiredArgsConstructor
 public class ProductsControllerAdvice {
+    private final MessageSource messageSource;
+
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<ExceptionDto> onProductNotFoundException(ProductNotFoundException e) {
-        return ResponseEntity.status(NOT_FOUND).body(new ExceptionDto("Product not found"));
+        String description = messageSource.getMessage(ProductNotFoundException.class.getSimpleName(), new Object[] {e.getId()}, Locale.getDefault());
+        return ResponseEntity.status(NOT_FOUND).body(new ExceptionDto(description));
     }
 }
