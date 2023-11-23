9fc7affbc306102fec2c9da9774597d6e79b5367
Marcin Chrost <mchrost82@gmail.com>
2023-11-23 11:20:09 +0100

    Event publisher & listener

--- src/main/java/eu/chrost/shop/payments/PaymentsConfiguration.java
+++ src/main/java/eu/chrost/shop/payments/PaymentsConfiguration.java
@@ -1,6 +1,7 @@
 package eu.chrost.shop.payments;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,12 +25,17 @@ class PaymentsConfiguration {
     }
 
     @Bean
-    public PaymentService fakePaymentService(@Qualifier("uuid") PaymentIdGenerator paymentIdGenerator, PaymentRepository paymentRepository) {
-        return new FakePaymentService(paymentIdGenerator, paymentRepository);
+    public PaymentService fakePaymentService(@Qualifier("uuid") PaymentIdGenerator paymentIdGenerator, PaymentRepository paymentRepository, ApplicationEventPublisher applicationEventPublisher) {
+        return new FakePaymentService(paymentIdGenerator, paymentRepository, applicationEventPublisher);
     }
 
     @Bean
     public PaymentConsoleLogger paymentConsoleLogger() {
         return new PaymentConsoleLogger();
+    }
+
+    @Bean
+    public PaymentStatusChangeListener paymentStatusChangeListener() {
+        return new PaymentStatusChangeListener();
     }
 }
