b4cf8d202ec4ae77949f6bba674d3841c3d0dc66
Marcin Chrost <mchrost82@gmail.com>
2023-11-23 11:26:30 +0100

    Event publisher & listener - async version

--- src/main/java/eu/chrost/shop/payments/PaymentStatusChangeListener.java
+++ src/main/java/eu/chrost/shop/payments/PaymentStatusChangeListener.java
@@ -2,15 +2,18 @@ package eu.chrost.shop.payments;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.scheduling.annotation.Async;
 
 @Slf4j
 public class PaymentStatusChangeListener implements ApplicationListener<PaymentStatusChangedEvent> {
 //    @EventListener
+//    @Async("threadPoolTaskExecutor")
 //    public void onPaymentStatusChange(PaymentStatusChangedEvent statusChangedEvent) {
 //        log.info("Payment changed status: {}", statusChangedEvent.getPayment());
 //    }
 
     @Override
+    @Async("threadPoolTaskExecutor")
     public void onApplicationEvent(PaymentStatusChangedEvent statusChangedEvent) {
         log.info("Payment changed status: {}", statusChangedEvent.getPayment());
     }
