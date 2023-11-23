dd0d941acd5f17b63d6f87392176dc5217f13598
Marcin Chrost <mchrost82@gmail.com>
2023-11-23 18:02:01 +0100

    Order audit system

--- src/main/java/eu/chrost/shop/orders/OrderService.java
+++ src/main/java/eu/chrost/shop/orders/OrderService.java
@@ -1,13 +1,20 @@
 package eu.chrost.shop.orders;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+
+import static eu.chrost.shop.orders.OrderAuditEventType.CREATE;
+import static eu.chrost.shop.orders.OrderAuditEventType.UPDATE;
 
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     public Order add(Order order) {
-        return orderRepository.save(order);
+        var savedOrder = orderRepository.save(order);
+        applicationEventPublisher.publishEvent(new OrderAuditEvent(this, CREATE, savedOrder));
+        return savedOrder;
     }
 
     public Order getBy(Long orderId) {
@@ -16,6 +23,7 @@ public class OrderService {
     }
 
     public void update(Order order) {
+        applicationEventPublisher.publishEvent(new OrderAuditEvent(this, UPDATE, order));
         orderRepository.update(order);
     }
 }
