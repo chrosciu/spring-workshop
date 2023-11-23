b28a52840cca9693cb511c4f32ce30a88c3beac0
Marcin Chrost <mchrost82@gmail.com>
2023-11-23 10:44:09 +0100

    Products and orders - Java config

---
+++ src/main/java/eu/chrost/shop/orders/OrdersConfiguration.java
@@ -0,0 +1,17 @@
+package eu.chrost.shop.orders;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrdersConfiguration {
+    @Bean
+    public OrderRepository orderRepository() {
+        return new HashMapOrderRepository();
+    }
+
+    @Bean
+    public OrderService orderService(OrderRepository orderRepository) {
+        return new OrderService(orderRepository);
+    }
+}
