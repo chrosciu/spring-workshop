b42e75cdfb757c32edae05119a4009449ff0fb1e
Marcin Chrost <mchrost82@gmail.com>
2023-11-24 11:21:31 +0100

    Product changed to entity

--- src/main/java/eu/chrost/shop/products/Product.java
+++ src/main/java/eu/chrost/shop/products/Product.java
@@ -1,16 +1,35 @@
 package eu.chrost.shop.products;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.math.BigDecimal;
 
+@Entity
 @Builder
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Product {
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
     private String description;
+    @Column(precision = 38)
     private BigDecimal price;
+    @Enumerated(EnumType.STRING)
     private ProductType type;
 }
