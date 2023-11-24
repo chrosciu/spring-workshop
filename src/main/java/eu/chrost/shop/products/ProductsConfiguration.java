98905d07f11d7a902f30322faf9c0fc6b1d8b0be
Marcin Chrost <mchrost82@gmail.com>
2023-11-24 11:21:31 +0100

    Product changed to entity

--- src/main/java/eu/chrost/shop/products/ProductsConfiguration.java
+++ src/main/java/eu/chrost/shop/products/ProductsConfiguration.java
@@ -5,10 +5,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ProductsConfiguration {
-    @Bean
-    public ProductRepository productRepository() {
-        return new HashMapProductRepository();
-    }
 
     @Bean
     public ProductService productService(ProductRepository productRepository) {
