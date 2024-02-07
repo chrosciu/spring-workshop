54c126ccb02889ff90d9625b2983474650134421
Marcin Chrost <mchrost82@gmail.com>
2024-02-07 10:12:54 +0100

    Queries for products

--- src/main/java/eu/chrost/shop/products/ProductRepository.java
+++ src/main/java/eu/chrost/shop/products/ProductRepository.java
@@ -1,6 +1,13 @@
 package eu.chrost.shop.products;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long>, CustomProductRepository {
+    List<Product> findByNameContaining(String name);
+
+    @Query("from Product p where p.type = :type")
+    List<Product> findByType(ProductType type);
 }
