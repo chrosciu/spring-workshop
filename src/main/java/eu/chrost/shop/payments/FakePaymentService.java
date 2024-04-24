26d2c329cb5698c72e47ecaaf15359feb950d3b0
Marcin Chrost <mchrost82@gmail.com>
2024-04-24 13:43:34 +0200

    @PostConstruct @PreDestroy

--- src/main/java/eu/chrost/shop/payments/FakePaymentService.java
+++ src/main/java/eu/chrost/shop/payments/FakePaymentService.java
@@ -1,13 +1,17 @@
 package eu.chrost.shop.payments;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import eu.chrost.shop.common.Retry;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class FakePaymentService implements PaymentService {
     private final PaymentIdGenerator paymentIdGenerator;
     private final PaymentRepository paymentRepository;
@@ -23,5 +27,15 @@ public class FakePaymentService implements PaymentService {
                 .status(PaymentStatus.STARTED)
                 .build();
         return paymentRepository.save(payment);
+    }
+
+    @PostConstruct
+    void init() {
+        log.info("Bean has been created");
+    }
+
+    @PreDestroy
+    void destroy() {
+        log.info("Bean is to be destroyed");
     }
 }
