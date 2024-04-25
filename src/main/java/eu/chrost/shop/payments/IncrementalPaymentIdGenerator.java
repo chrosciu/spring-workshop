1651ed4d660fd7effe6ba600b0150d5faab7d46d
Marcin Chrost <mchrost82@gmail.com>
2024-04-25 11:40:25 +0200

    Inject properties with @ConfigurationProperties

--- src/main/java/eu/chrost/shop/payments/IncrementalPaymentIdGenerator.java
+++ src/main/java/eu/chrost/shop/payments/IncrementalPaymentIdGenerator.java
@@ -9,10 +9,13 @@ public class IncrementalPaymentIdGenerator implements PaymentIdGenerator {
 
     @Setter
     private long index;
+    @Setter
+    private long step;
 
     @Override
     public String getNext() {
-        return String.format(ID_FORMAT, ++index);
+        index = index + step;
+        return String.format(ID_FORMAT, index);
     }
 
 }
--- src/main/java/eu/chrost/shop/payments/PaymentsConfiguration.java
+++ src/main/java/eu/chrost/shop/payments/PaymentsConfiguration.java
@@ -1,18 +1,20 @@
 package eu.chrost.shop.payments;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(IncrementalPaymentIdGeneratorProperties.class)
 class PaymentsConfiguration {
     @Bean
     @ConditionalOnProperty(value = "uuid.generator.enabled", havingValue = "false")
-    public PaymentIdGenerator incrementalPaymentIdGenerator(@Value("${incremental.generator.initial:10}") long initialValue) {
+    public PaymentIdGenerator incrementalPaymentIdGenerator(IncrementalPaymentIdGeneratorProperties generatorProperties) {
         var generator = new IncrementalPaymentIdGenerator();
-        generator.setIndex(initialValue);
+        generator.setIndex(generatorProperties.initial());
+        generator.setStep(generatorProperties.step());
         return generator;
     }
 
