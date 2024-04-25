c8930343840788614b85c6d9f4a59fd91fe8ddf1
Marcin Chrost <mchrost82@gmail.com>
2024-04-25 11:40:25 +0200

    Inject properties with @ConfigurationProperties

--- src/main/java/eu/chrost/shop/payments/IncrementalPaymentIdGenerator.java
+++ src/main/java/eu/chrost/shop/payments/IncrementalPaymentIdGenerator.java
@@ -7,10 +7,13 @@ public class IncrementalPaymentIdGenerator implements PaymentIdGenerator {
 
     @Setter
     private long index;
+    @Setter
+    private long step;
 
     @Override
     public String getNext() {
-        return String.format(ID_FORMAT, ++index);
+        index = index + step;
+        return String.format(ID_FORMAT, index);
     }
 
 }
