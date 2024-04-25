a145a8577dfd3cf5a8444eb11842c4752272e278
Marcin Chrost <mchrost82@gmail.com>
2024-04-25 11:59:09 +0200

    ConfigurationProperties validation (with constructor and hibernate validator)

--- src/main/java/eu/chrost/shop/payments/IncrementalPaymentIdGeneratorProperties.java
+++ src/main/java/eu/chrost/shop/payments/IncrementalPaymentIdGeneratorProperties.java
@@ -1,6 +1,15 @@
 package eu.chrost.shop.payments;
 
+import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "incremental.generator")
-public record IncrementalPaymentIdGeneratorProperties(long initial, long step) {}
+@Validated
+public record IncrementalPaymentIdGeneratorProperties(@Positive long initial, @Positive long step) {
+//    public IncrementalPaymentIdGeneratorProperties {
+//        if (initial <= 0) {
+//            throw new IllegalArgumentException("Must be positive");
+//        }
+//    }
+}
