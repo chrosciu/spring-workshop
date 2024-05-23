528dc26d07596876ecf48dcd57b32967d370e2ab
Marcin Chrost <mchrost82@gmail.com>
2024-05-23 09:42:49 +0200

    @ConfigurationProperties validation

--- src/main/java/eu/chrost/shop/payments/IncrementalPaymentIdGeneratorProperties.java
+++ src/main/java/eu/chrost/shop/payments/IncrementalPaymentIdGeneratorProperties.java
@@ -1,6 +1,9 @@
 package eu.chrost.shop.payments;
 
+import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "incremental.generator")
-public record IncrementalPaymentIdGeneratorProperties(long initial, long step) {}
+@Validated
+public record IncrementalPaymentIdGeneratorProperties(@Positive long initial, @Positive long step) {}
