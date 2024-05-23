be87a0d2b355091fe129f5428646c9ee3d109784
Marcin Chrost <mchrost82@gmail.com>
2024-05-23 19:55:15 +0200

    Query methods in ProductRepository + tests

--- src/main/java/eu/chrost/shop/products/ProductRepository.java
+++ src/main/java/eu/chrost/shop/products/ProductRepository.java
@@ -1,5 +1,12 @@
 package eu.chrost.shop.products;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByNameContaining(String name);
+    Page<Product> findByNameContaining(String name, Pageable pageable);
+}
